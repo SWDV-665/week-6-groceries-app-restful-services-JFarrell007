c700774c330dd91fc2f5f2987fe92b1c9bfceaac
Jim Farrell <jimmyfarrell007@gmail.com>
2019-08-03 14:21:36 -0400

    Initial Checkin

---
+++ groceries/src/app/api/groceries.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { throwError, concat, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GroceriesService {
+  items: any = [];
+  dataChanged$: Observable<boolean>;
+  private dataChangeSubject: Subject<boolean>;
+  baseURL = 'http://localhost:8080';
+
+  constructor(public http: HttpClient) {
+    console.log('Hello GroceriesService Provider');
+
+    this.dataChangeSubject = new Subject<boolean>();
+    this.dataChanged$ = this.dataChangeSubject.asObservable();
+  }
+
+  getItems(): Observable<object[]> {
+    console.log('getItems() called .....');
+    return this.http.get<object[]>(this.baseURL + '/api/groceries').pipe(
+      catchError(this.handleError)
+      );
+  }
+  private handleError(error: Response | any) {
+    let errMsg: string;
+    if (error instanceof Response) {
+      const err = error || '';
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
+  removeItem(id) {
+    console.log('#### Remove Item - Id = ', id);
+    this.http.delete(this.baseURL + '/api/groceries/' + id).subscribe(res => {
+      this.items = res;
+      this.dataChangeSubject.next(true);
+      // window.location.reload();
+    });
+  }
+
+  addItem(item) {
+    this.http.post(this.baseURL + '/api/groceries', item).subscribe(res => {
+      this.items = res;
+      this.dataChangeSubject.next(true);
+      // window.location.reload();
+    });
+  }
+
+  editItem(item, index) {
+    console.log('editItem ' + item.id);
+    console.log('editItem '  + item);
+    this.http.put(this.baseURL + '/api/groceries/' + item.id, item).subscribe(res => {
+      this.items = res;
+      this.dataChangeSubject.next(true);
+      // window.location.reload();
+    });
+  }
+}
